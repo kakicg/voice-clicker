ff859982ca5aeecf36a991dd69d3ab75be53ae09
kakicg <kakicg@mac.com>
2026-05-03 15:16:03 +0900

    Initial Commit

---
+++ VoiceClicker/VoiceClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceClickerApp.swift
+//  VoiceClicker
+//
+//  Created by 柿山幹雄 on 2026/05/03.
+//
+
+import SwiftUI
+
+@main
+struct VoiceClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
